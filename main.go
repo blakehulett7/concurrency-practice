ca53a4f9b5b34f20d970206046bdc2262c02dad8
blakehulett7 <blake.hulett7@gmail.com>
2025-08-13 08:57:07 -0500

    checkpoint... a waitgroup would make sense here it seems!

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	go PrintThis("In Nomine Patris...")
+	hail_mary := []string{
+		"Ave Maria, gratia plena",
+		"Dominus tecum",
+		"benedicta tu in mulieribus",
+		"et benedictus fructus ventris tui, Iesus.",
+		"Sacta Maria mater dei,",
+		"ora pro nobis preccatoribus",
+		"nunc et in hora a mortis nostrae",
+		"Amen",
+	}
+
+	for i, phrase := range hail_mary {
+		go PrintThis(fmt.Sprintf("%d. %s\n", i, phrase))
+	}
+
+	PrintThis("In Nomine Patris...")
 	PrintThis("Et Filii...")
 	PrintThis("Et Spiritus Sancti!")
 }
