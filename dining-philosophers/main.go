1665cfc1e33f3d7780c679168eabbdc0da94f5ac
blakehulett7 <blake.hulett7@gmail.com>
2025-08-14 15:29:39 -0500

    nice

--- dining-philosophers/main.go
+++ dining-philosophers/main.go
@@ -53,4 +53,43 @@ func main() {
 
 func Dine(philosopher Philosopher, eating, seated *sync.WaitGroup, forks map[int]*sync.Mutex) {
 	defer eating.Done()
+
+	fmt.Printf("%s is seated at the table.\n", philosopher.Name)
+	seated.Done()
+
+	seated.Wait()
+
+	for i := Hunger; i > 0; i-- {
+		if philosopher.LeftFork > philosopher.RightFork {
+
+			forks[philosopher.RightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.Name)
+
+			forks[philosopher.LeftFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.Name)
+
+		} else {
+
+			forks[philosopher.LeftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.Name)
+
+			forks[philosopher.RightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.Name)
+
+		}
+
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.Name)
+		time.Sleep(EatTime)
+
+		fmt.Printf("\t%s is thinking.\n", philosopher.Name)
+		time.Sleep(ThinkTime)
+
+		forks[philosopher.LeftFork].Unlock()
+		forks[philosopher.RightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.Name)
+	}
+
+	fmt.Printf("%s is satisfied.\n", philosopher.Name)
+	fmt.Printf("%s left the table.\n", philosopher.Name)
 }
