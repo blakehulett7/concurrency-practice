8d9d9c640c3d6b9ffa1554734cc5f7277195526b
blakehulett7 <blake.hulett7@gmail.com>
2025-08-14 15:18:13 -0500

    structure created... time for the logic

--- dining-philosophers/main.go
+++ dining-philosophers/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+type Philosopher struct {
+	Name      string
+	LeftFork  int
+	RightFork int
+}
+
+var Philosophers = []Philosopher{
+	{Name: "Jesus Christ", LeftFork: 4, RightFork: 0},
+	{Name: "Thomas Acquinas", LeftFork: 0, RightFork: 1},
+	{Name: "CS Lewis", LeftFork: 1, RightFork: 2},
+	{Name: "JRR Tolkien", LeftFork: 2, RightFork: 3},
+	{Name: "Augustine", LeftFork: 3, RightFork: 4},
+}
+
+var Hunger = 3
+var EatTime = time.Second
+var ThinkTime = 3 * time.Second
+var SleepTime = time.Second
+
+func main() {
+	fmt.Println("Dominus Iesus Christus")
+	fmt.Println("----------------------")
+	fmt.Println()
+	fmt.Println("The table is empty")
+
+	eating := &sync.WaitGroup{}
+	seated := &sync.WaitGroup{}
+
+	eating.Add(len(Philosophers))
+	seated.Add(len(Philosophers))
+
+	forks := map[int]*sync.Mutex{}
+	for i := 0; i < len(Philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	for _, philosopher := range Philosophers {
+		go Dine(philosopher, eating, seated, forks)
+	}
+	eating.Wait()
+
+	fmt.Println("The table is empty")
+
+}
+
+func Dine(philosopher Philosopher, eating, seated *sync.WaitGroup, forks map[int]*sync.Mutex) {
+	defer eating.Done()
 }
