a18947e349467a9c8adfde8d77720b38e12ce5f3
blakehulett7 <blake.hulett7@gmail.com>
2025-08-18 15:34:51 -0500

    i generally like this guy, but this nested function hell is a bit batshit...

---
+++ cub/data.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type UemplateData struct {
+	StringMap     map[string]string
+	IntMap        map[string]int
+	FloatMap      map[string]float64
+	Data          map[string]any
+	Flash         string
+	Warning       string
+	Error         string
+	Authenticated int
+	Now           time.Time
+}
--- cub/home.go
+++ cub/home.go
@@ -1,7 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func (app *Bridge) Home(w http.ResponseWriter, r *http.Request) {
-	panic("hi")
+	t := template.Must(template.ParseGlob("templates/*.gohtml"))
+
+	t.ExecuteTemplate(w, "home.page.gohtml", nil)
 }
